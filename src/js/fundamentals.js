be24621112843a6f20a134c3d49afa6f8045ce5b
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-12-10 17:09:17 -0300

    feat: added javascript fundamentals

---
+++ src/js/fundamentals.js
@@ -0,0 +1,30 @@
+// variables
+
+let variable = 'sou alteravel'
+const constant = 'não é redeclaravel'
+
+variavel = 1
+// proibido reassinar uma constante
+// constant = 2
+
+let maria
+
+//console
+console.log(variable)
+console.error(constant)
+console.info('Sou uma informação')
+console.warn('Var é depreciado')
+
+// tipos de dados 
+const lastName = 'Nicacio'
+let age = 25
+const person = {
+    name: 'joao',
+    age: 16,
+    gender: 'male',
+    sexo: "Masculino"
+}
+
+const fruits = ['morango', 'laranja', 'uva', 'jamelão', 'kiwi', 'jambo']
+console.table(fruits)
+console.log(person)
