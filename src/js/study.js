6becd4b7d6e950d5789ce8ea321b53476d932048
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-12-15 10:56:26 -0300

    feat: exercises module 3

--- src/js/study.js
+++ src/js/study.js
@@ -437,3 +437,22 @@ let e = false
 console.log(e)
 e ||= true
 console.log(e)
+
+//Exercicio 1
+console.log(2*3+1)
+console.log(2**4)
+console.log(5*1)
+console.log(8 ** 2 - 5 ** 2)
+
+//Exercicio 2
+console.log(4 * 5 === 20)
+console.log(6 * 5 === 30)
+console.log(-17 != 0) // -17 < 0
+console.log(25 != 1) // 25 > 1
+console.log(2 + 2 * 2 != 4)
+
+//Exercico 3 
+console.log(true || false)
+console.log(false === false) // || !
+console.log(false || false || true)
+console.log(true || false && false && true)
